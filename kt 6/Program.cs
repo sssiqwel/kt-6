2f019c5f0e5efd728979dfa0f662ba7fd4216222
User <User@DESKTOP-75A86ME>
2025-09-29 11:31:35 +0300

    Добавьте файлы проекта.

---
+++ kt 6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+enum DayOfWeek
+{
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PrintDayInRussian(DayOfWeek.Monday);
+        PrintDayInRussian(DayOfWeek.Tuesday);
+        PrintDayInRussian(DayOfWeek.Wednesday);
+        PrintDayInRussian(DayOfWeek.Thursday);
+        PrintDayInRussian(DayOfWeek.Friday);
+        PrintDayInRussian(DayOfWeek.Saturday);
+        PrintDayInRussian(DayOfWeek.Sunday);
+    }
+    static void PrintDayInRussian(DayOfWeek day)
+    {
+        string russianDay = day switch
+        {
+            DayOfWeek.Monday => "Понедельник",
+            DayOfWeek.Tuesday => "Вторник",
+            DayOfWeek.Wednesday => "Среда",
+            DayOfWeek.Thursday => "Четверг",
+            DayOfWeek.Friday => "Пятница",
+            DayOfWeek.Saturday => "Суббота",
+            DayOfWeek.Sunday => "Воскресенье",
+            _ => "Неизвестный день"
+        };
+
+        Console.WriteLine(russianDay);
+    }
+}
